9d974116562a0ca983495df690fdc4457e49831a
sey323 <sey323_nm@yahoo.co.jp>
2024-08-15 17:49:51 +0900

    :recycle: 記事執筆の際のリファクタリング反映

--- utils/wagmi.tsx
+++ utils/wagmi.tsx
@@ -4,29 +4,28 @@ import { SepoliaTestnet } from "./chains/SepoliaTestnet";
 import { intmaxwalletsdk } from "intmax-walletsdk/rainbowkit";
 import { createConfig, http } from "wagmi";
 import { walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
+import { polygon } from "viem/chains";
 
 // Intmax Walletの設定
-const wallets = [
-  intmaxwalletsdk({
-    wallet: {
-      url: "https://wallet.intmax.io/",
-      name: "INTMAX Wallet",
-      iconUrl: "https://wallet.intmax.io/favicon.ico",
-    },
-    metadata: {
-      name: "IntmaxWallet Scroll Connect Sample App",
-      description:
-        "This is a sample of connecting to the scroll testnet using intmax Wallet.",
-    },
-  }),
-];
+const intmaxWallet = intmaxwalletsdk({
+  wallet: {
+    url: "https://wallet.intmax.io/",
+    name: "INTMAX Wallet",
+    iconUrl: "https://wallet.intmax.io/favicon.ico",
+  },
+  metadata: {
+    name: "IntmaxWallet Connect Sample App",
+    description:
+      "This is a sample of connecting to the polygon using intmax Wallet.",
+  },
+});
 
 // 接続可能なウォレットの設定
 const connectors = connectorsForWallets(
   [
     {
       groupName: "Recommended Wallet",
-      wallets,
+      wallets: [intmaxWallet],
     },
     {
       groupName: "Popular Wallets",
