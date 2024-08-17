0b76a25f70e7a5d7352c73dc80ffc96d405a8ec1
sey323 <sey323_nm@yahoo.co.jp>
2024-08-17 12:32:28 +0900

    :+1: 利用するコントラクトの変更

--- app/components/read-contract.tsx
+++ app/components/read-contract.tsx
@@ -1,19 +1,29 @@
 "use client";
 import { useReadContract } from "wagmi";
-import { SimpleNftDropAbi } from "@/utils/contracts/simple-nft-drop";
+import { WagmiSample } from "@/utils/contracts/wagmi-sample";
 
 export function ReadContract() {
-  const result = useReadContract({
-    abi: SimpleNftDropAbi,
-    address: "0xe22E258bF0B9E24AedC301b01A3643a7A082aDAd",
-    functionName: "totalMinted",
+  const name = useReadContract({
+    abi: WagmiSample,
+    address: "0xa9c12F47836207879Ec1bbec09361DB932Db2f09",
+    functionName: "name",
+  });
+
+  const totalSupply = useReadContract({
+    abi: WagmiSample,
+    address: "0xa9c12F47836207879Ec1bbec09361DB932Db2f09",
+    functionName: "totalSupply",
   });
 
   return (
-    <div>
-      {result.isLoading && <div>Loading...</div>}
-      {result.isSuccess && (
-        <div>totalMinted: {result.data?.toString()}です</div>
+    <div className="w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <h1 className="flex justify-center font-semibold text-gray-900 dark:text-white">
+        コントラクト名: {name.data}
+      </h1>
+
+      {totalSupply.isLoading && <div>Loading...</div>}
+      {totalSupply.isSuccess && (
+        <div>トークンの発行量は {totalSupply.data?.toString()}です</div>
       )}
     </div>
   );
