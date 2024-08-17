2debeff6787074337d22b403722b7fa4e478fd09
sey323 <sey323_nm@yahoo.co.jp>
2024-08-17 12:40:16 +0900

    :+1: ERC20のトークンを発行する処理

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { ReadContract } from "./components/read-contract";
+import { MintNFT } from "./components/write-mint";
+
 function Page() {
-  return <div>top</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-center my-3">
+        <ReadContract></ReadContract>
+      </div>
+
+      <div className="flex items-center justify-center my-4">
+        <MintNFT></MintNFT>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
