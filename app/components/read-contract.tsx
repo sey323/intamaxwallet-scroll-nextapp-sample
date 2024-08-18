7d8408e0a9310f041917cfb61eb328fe78ea446c
sey323 <sey323_nm@yahoo.co.jp>
2024-08-18 15:26:33 +0900

    :+1: 最新のNFTの文字列を表示

--- app/components/read-contract.tsx
+++ app/components/read-contract.tsx
@@ -15,6 +15,13 @@ export function ReadContract({ address }: { address: `0x${string}` }) {
     functionName: "totalSupply",
   });
 
+  const latestNftSentence = useReadContract({
+    abi: WagmiSample,
+    address: address,
+    functionName: "getSentence",
+    args: [BigInt(totalSupply.data?.toString() || "0") - BigInt(1)],
+  });
+
   return (
     <div className="w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <h1 className="flex justify-center font-semibold text-gray-900 dark:text-white">
@@ -27,6 +34,15 @@ export function ReadContract({ address }: { address: `0x${string}` }) {
       {totalSupply.isSuccess && (
         <div>トークンの発行量は {totalSupply.data?.toString()}です</div>
       )}
+
+      <div className="my-4 border-b border-gray-300"></div>
+
+      <div>
+        <h1 className="flex justify-center font-semibold text-gray-900 dark:text-white">
+          最後に発行されたNFTに登録された文字列
+        </h1>
+        <div>{latestNftSentence.data}</div>
+      </div>
     </div>
   );
 }
